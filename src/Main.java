acf450292cf6d3d42d1efb417569870498027d92
A-Machi <aymanmachi32@gmail.com>
2020-10-24 11:46:29 +0200

    Titel gesetzt Größe definiert Position des Fensteres gesetzt (center) Exit Ausführung gesetzt Anschließend alles sichtbar gemacht

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main(){
+        setTitle("Schiffe Versenken");
+        setSize(600, 450);  // größe des Fensters fest legen
+        setLocationRelativeTo(null); //Setzt das Fenster in der Mitte
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Schließst das Fenster
+    }
+
     public static void main(String[] args) {
-        System.out.println("Start");
+        EventQueue.invokeLater(new Runnable() {     //Ereignisbehandlung
+            @Override
+            public void run() {
+                Main m = new Main();
+                m.setVisible(true);             //macht das Fenster mit den Info aus der Klasse Main sichtbar
+            }
+        });
     }
 }
 
