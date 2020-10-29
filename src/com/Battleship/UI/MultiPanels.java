94460fe99e0fbdaf69c80919f1227cdf7f32a7a6
Ali Karami <kaali728@yahoo.com>
2020-10-29 14:19:28 +0100

    add cardlayout

--- src/com/Battleship/UI/MultiPanels.java
+++ src/com/Battleship/UI/MultiPanels.java
@@ -1,40 +1,49 @@
 package com.Battleship.UI;
 
+import javafx.scene.layout.Pane;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MultiPanels implements ActionListener {
+public class MultiPanels extends JFrame implements ActionListener {
 
-    JFrame window = new JFrame();
-    JPanel panel1 = new JPanel();
-    JPanel panel2= new JPanel();
+    JPanel panel1;
+    JPanel GamePanel;
+    JPanel MenuPanel;
+
     JButton click = new JButton("Click");
-    JLabel text = new JLabel("Button clicked");
+    CardLayout cardLayout;
 
     MultiPanels(){
+        cardLayout = new CardLayout();
+        panel1 = new JPanel(cardLayout);
         panel1.setBackground(Color.red);
-        panel2.setBackground(Color.green);
-        panel2.add(click);
-        text.setForeground(Color.DARK_GRAY);
-        panel1.add(text);
-        text.setVisible(false);
+        panel1.add(click);
+        GamePanel = new JPanel();
+        MenuPanel = new JPanel();
+        panel1.add(GamePanel, "game");
+        panel1.add(MenuPanel, "menu");
+
 
         click.addActionListener(this);
 
-        window.add(panel1, BorderLayout.CENTER);
-        window.add(panel2, BorderLayout.PAGE_END);
-        window.setSize(800,400);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setVisible(true);
+        add(panel1);
+        add(click, BorderLayout.SOUTH);
+        pack();
+        setSize(800,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setVisible(true);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        text.setVisible(true);
-        panel1.setBackground(Color.blue);
-        panel2.setBackground(Color.white);
+        gameOn();
+    }
+    public void gameOn(){
+        cardLayout.show(panel1, "panel");
     }
 }
