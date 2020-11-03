c44cc1451b8c4b16b356de64abe87902c7998ad0
Ali Karami <kaali728@yahoo.com>
2020-11-03 20:13:48 +0100

    board added

--- src/com/Battleship/Model/Board.java
+++ src/com/Battleship/Model/Board.java
@@ -1,15 +1,29 @@
 package com.Battleship.Model;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Board extends Canvas {
     int size=0;
+    Object[][] gridArray;
     public Board(int size){
         this.size=size;
+        gridArray = new Object[size][size];
         initlayout();
     }
     public void initlayout(){
-        setBackground(Color.BLUE);
+        setBackground(Color.DARK_GRAY);
         setSize(new Dimension(500,500));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int i=0; i< gridArray.length; i++){
+            for (int j=0; j<gridArray[i].length; j++){
+                gridArray[i][j] = null;
+                g.fillRect(i*(this.getWidth()/size), j*(this.getHeight()/size), (this.getWidth()/size)-2,(this.getHeight()/size)-2);
+            }
+        }
     }
 }
--- src/com/Battleship/UI/ShipSetupScreen.java
+++ src/com/Battleship/UI/ShipSetupScreen.java
@@ -21,75 +21,13 @@ public class ShipSetupScreen extends Panel {
     }
 
     public void initlayout() {
-//        Box hbox = Box.createHorizontalBox();
-//        {
-//            hbox.add(Box.createHorizontalStrut(10));
-//            hbox.add(new Board(10));
-//            hbox.add(Box.createHorizontalStrut(10));
-//        }
-//        add(hbox);
-//        hbox.setAlignmentY(Component.CENTER_ALIGNMENT);
-
-//        jTable1 = new javax.swing.JTable();
-//
-//        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-//                new Object [][] {
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                        {null, null, null, null, null},
-//                },
-//                new String [] {
-//                        "", "", "", "", ""
-//                }
-//        ));
-//
-//        jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
-//
-//        jTable1.setAutoscrolls(false);
-//
-//        jTable1.setCellSelectionEnabled(true);
-//
-//        jTable1.setFillsViewportHeight(true);
-//
-//        jTable1.setMinimumSize(new java.awt.Dimension(200, 300));
-//
-//        jTable1.setName(""); // NOI18N
-//
-//        jTable1.setRowHeight(50);
-//        jTable1.getColumnModel().getColumn(0).setPreferredWidth(50);
-//        jTable1.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-//
-//        add(jTable1);
-
+        Box hbox = Box.createHorizontalBox();
+        {
+            hbox.add(Box.createHorizontalStrut(10));
+            hbox.add(new Board(7));
+            hbox.add(Box.createHorizontalStrut(10));
+        }
+        add(hbox);
+        hbox.setAlignmentY(Component.CENTER_ALIGNMENT);
     }
-
-
 }
