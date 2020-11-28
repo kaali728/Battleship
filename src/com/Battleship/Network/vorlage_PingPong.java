bb71d087c58f63e53fb637021650ea65a1634441
Christian Miller <syrex.portstein@gmail.com>
2020-11-28 02:20:33 +0100

    Ping Pong.

--- src/com/Battleship/Network/vorlage_PingPong.java
+++ src/com/Battleship/Network/vorlage_PingPong.java
@@ -1,19 +1,30 @@
 package com.Battleship.Network;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class Client {
+// Beispielprogramm mit graphischer Oberfläche und Socketverbindung.
+// Ein Aufruf ohne Kommandozeilenargumente startet das Programm als Server.
+// Ein Aufruf mit einem Rechnernamen/IP-Adresse startet das Programm als Client,
+// der sich mit dem Server auf diesem Rechner verbindet.
+// In beiden Fällen wird lediglich ein Knopf angezeigt,
+// der abwechselnd aktiviert und deaktiviert wird,
+// sodass er immer nur auf einer "Seite" gedrückt werden kann.
+public class vorlage_PingPong {
     // Variablen, die im gesamten Programm benötigt werden.
+    private static String role;		// Rolle: Server oder Client.
+    private static BufferedReader in;	// Verpackung des Socket-Eingabestroms.
     private static Writer out;		// Verpackung des Socket-Ausgabestroms.
     private static JButton button;	// Der o. g. Knopf.
 
     // Graphische Oberfläche aufbauen und anzeigen.
     private static void startGui () {
         // Hauptfenster mit Titelbalken etc. (JFrame) erzeugen.
-        JFrame frame = new JFrame("Client");
+        JFrame frame = new JFrame(role);
 
         // Beim Schließen des Fensters (z. B. durch Drücken des
         // X-Knopfs in Windows) soll das Programm beendet werden.
@@ -28,7 +39,7 @@ public class Client {
         frame.add(Box.createGlue());
 
         // Horizontal zentrierten Knopf (JButton) hinzufügen.
-        button = new JButton("Client");
+        button = new JButton(role);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         button.addActionListener(
                 // Wenn der Knopf gedrückt wird,
@@ -37,7 +48,7 @@ public class Client {
                 // und eine beliebige Nachricht an die andere "Seite" geschickt,
                 // damit diese ihren Knopf aktivieren kann.
                 (e) -> {
-                    System.out.println("Client clicked the button.");
+                    System.out.println(role);
                     button.setEnabled(false);
                     try {
                         out.write(String.format("%s%n", "message"));
@@ -50,6 +61,9 @@ public class Client {
         );
         frame.add(button);
 
+        // Der Server-Knopf soll anfangs deaktiviert sein.
+        if (role.equals("Server")) button.setEnabled(false);
+
         // Dehnbaren Zwischenraum am unteren Rand hinzufügen.
         frame.add(Box.createGlue());
 
@@ -59,25 +73,51 @@ public class Client {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 5000);
+    // Hauptprogramm.
+    public static void main (String [] args) throws IOException {
+        // Verwendete Portnummer (vgl. Server.java).
+        final int port = 5000;
 
-        // Send message to server.
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-        printWriter.println("Hello, I am a message from the client.");
-        printWriter.flush();
+        // Socketverbindung zur anderen "Seite" herstellen.
+        Socket s;
+        if (args.length == 1) {
+            role = "Server";
 
-        // Get message from server.
-        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        out = new OutputStreamWriter(socket.getOutputStream());
+            // Die eigene(n) IP-Adresse(n) ausgeben,
+            // damit der Benutzer sie dem Benutzer des Clients mitteilen kann.
+            System.out.print("My IP address(es):");
+            Enumeration<NetworkInterface> nis =
+                    NetworkInterface.getNetworkInterfaces();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                Enumeration<InetAddress> ias = ni.getInetAddresses();
+                while (ias.hasMoreElements()) {
+                    InetAddress ia = ias.nextElement();
+                    if (!ia.isLoopbackAddress()) {
+                        System.out.print(" " + ia.getHostAddress());
+                    }
+                }
+            }
+            System.out.println();
+            System.out.println("Waiting for client connection ...");
 
-        String str = bufferedReader.readLine();
-        System.out.println("[Server]: " + str);
+            ServerSocket ss = new ServerSocket(port);
+            s = ss.accept();
+        }
+        else {
+            role = "Client";
+            s = new Socket("localhost", 5000);
+        }
+        System.out.println("Connection established.");
+
+        // Ein- und Ausgabestrom des Sockets ermitteln
+        // und als BufferedReader bzw. Writer verpacken.
+        in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        out = new OutputStreamWriter(s.getOutputStream());
 
         // Graphische Oberfläche aufbauen.
         SwingUtilities.invokeLater(
-                Client::startGui
+                () -> { startGui(); }
         );
 
         // Netzwerknachrichten lesen und verarbeiten.
@@ -86,17 +126,17 @@ public class Client {
         // Manipulationen an der Oberfläche sollten aber mittels invokeLater
         // (oder invokeAndWait) ausgeführt werden.
         while (true) {
-            String line = bufferedReader.readLine();
+            String line = in.readLine();
             if (line == null) break;
             SwingUtilities.invokeLater(
-                    () -> button.setEnabled(true)
+                    () -> { button.setEnabled(true); }
             );
         }
 
         // EOF ins Socket "schreiben" und das Programm explizit beenden
         // (weil es sonst weiterlaufen würde, bis der Benutzer das Hauptfenster
         // schließt).
-        socket.shutdownOutput();
+        s.shutdownOutput();
         System.out.println("Connection closed.");
         System.exit(0);
     }
